37b15397e475b636480508d567317ace2628af7b
tadmisha <tadmisha@gmail.com>
2025-01-14 01:45:41 +0400

    Works, need only to add inputs (some day)

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ load_dotenv()
 # * Token of the telegram bot
 TOKEN = os.getenv("TOKEN")
 
+# * Url of the product and all the filters
+url = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=s9+fe&_sop=15&_ipg=240&_stpos=19720"
+
 # & Converting string price to float
 def to_float(num_str: str) -> float:
     num_float = ''.join(char for char in num_str if char.isdigit() or char in ".,-")
@@ -45,10 +48,11 @@ def get_listings(url: str) -> list:
                 if len(listing.find_all("span", {"class": "s-item__price"})) > 1:
                     format =  3
                 format =  2
+                listings[-1]["time_left"] = listing.find("span", {"class": "s-item__time-left"}).text[:-5]
 
             listings[-1]["format"] = format
             try: listings[-1]["price"] = [to_float(price.text.split("to")[0]) for price in listing.find_all("span", {"class": "s-item__price"})]
-            except ValueError: ... # ~ The "tap to see price" listings
+            except ValueError: del listings[-1] # ~ The "tap to see price" listings
 
         else:
             break
@@ -57,22 +61,40 @@ def get_listings(url: str) -> list:
 
 
 # & Finding offers with given max price
-def find_offers(auctions: list[str], max_price: int):
-    ...
-        
+def find_offers(listings: list[dict], max_price: int):
+    offers = []
+    for listing in listings:
+        price = listing["price"][0]
+        print(price)
+        if listing["format"] == 1:
+            if price <= max_price:
+                offers.append(listing["url"])
+        elif listing["format"] == 2:
+            if price <= max_price and ('m' in listing["time_left"] and not 'h' in listing["time_left"]) or ('s' in listing["time_left"] and not 'm' in listing["time_left"]):
+                if ('s' in price) or ('m' in price and int(price[:-1]) < 30):
+                    offers.append(listing["url"])
+        elif listing["format"] == 3:
+            if listing["price"][1] <= max_price:
+                offers.append(listing["url"])
+    return offers
+
 
 # & Checks if function found anything every n period of time and notifies if it does
 async def check_function(context: CallbackContext):
     job = context.job
     chat_id = job.data 
-    if find_offers():
-        await context.bot.send_message(chat_id=chat_id, text="Found!")
+    listings = get_listings(url)
+    urls = find_offers(listings, 200)
+    if urls:
+        await context.bot.send_message(chat_id=chat_id, text="Found!, check the links: " + ",  ".join(urls))
+
 
 # & /start function
 async def start(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
     context.job_queue.run_repeating(check_function, interval=30, first=0, data=chat_id)
     await update.message.reply_text('Bot started! It will check the product every 30 seconds.')
+
 
 # & Main function
 def main():
