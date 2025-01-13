edd1ac88401e6fad6e945233018842e2c344ab05
tadmisha <111607554+tadmisha@users.noreply.github.com>
2025-01-14 02:52:54 +0400

    Forgot to import regex

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext, Job
+import re
 
 load_dotenv()
 
